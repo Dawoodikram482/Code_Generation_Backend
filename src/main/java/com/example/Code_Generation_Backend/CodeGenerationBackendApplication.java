85535890aa4553f55cf6fd8a43c95a140578d42e
Dawoodikram482 <mianmdawood482@gmail.com>
2024-04-22 16:16:26 +0200

    first commit

---
+++ src/main/java/com/example/Code_Generation_Backend/CodeGenerationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.Code_Generation_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeGenerationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeGenerationBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Code_Generation_Backend/CodeGenerationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Code_Generation_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeGenerationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
