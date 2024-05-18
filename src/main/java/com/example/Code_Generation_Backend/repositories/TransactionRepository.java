da9f5e8522c764451102aaaf234929ae49ff9538
Dawoodikram482 <mianmdawood482@gmail.com>
2024-05-18 19:27:03 +0200

    Api Design added, transactions in progress

--- src/main/java/com/example/Code_Generation_Backend/repositories/TransactionRepository.java
+++ src/main/java/com/example/Code_Generation_Backend/repositories/TransactionRepository.java
@@ -3,7 +3,6 @@ package com.example.Code_Generation_Backend.repositories;
 import com.example.Code_Generation_Backend.models.Transaction;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -15,6 +14,6 @@ import java.time.LocalDate;
 public interface TransactionRepository extends CrudRepository<Transaction, Long>,
     JpaSpecificationExecutor<Transaction> {
 
-  @Query("SELECT t FROM Transaction t WHERE t.accountFrom.customer.email = :email")
-  Page<Transaction> getTransactionsByCustomer(Pageable pageable, String email);
+  @Query("SELECT t FROM Transaction t WHERE t.accountFrom.iban = :iban")
+  Page<Transaction> getTransactionsByCustomer(Pageable pageable, String iban);
 }
