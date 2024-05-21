071812604d239f473ddd4c5bbf055e2c6855ea05
Dawoodikram482 <mianmdawood482@gmail.com>
2024-05-21 16:55:31 +0200

    new updates

--- src/main/java/com/example/Code_Generation_Backend/services/UserService.java
+++ src/main/java/com/example/Code_Generation_Backend/services/UserService.java
@@ -64,6 +64,7 @@ public class UserService {
         } else {
             throw new IllegalArgumentException("User not found");
         }
+    }
     public User getUserByEmail(String email){
         return userRepository.findByEmail(email).orElseThrow(()->new EntityNotFoundException("User with email: "+email+" not found"));
     }
