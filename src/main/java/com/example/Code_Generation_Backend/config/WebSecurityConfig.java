21b8813a44f97824085a0f13bdce6028b3a3aa5b
Dawoodikram482 <mianmdawood482@gmail.com>
2024-06-09 19:06:34 +0200

    deployed frontend pages link added to cors configuration

--- src/main/java/com/example/Code_Generation_Backend/config/WebSecurityConfig.java
+++ src/main/java/com/example/Code_Generation_Backend/config/WebSecurityConfig.java
@@ -34,7 +34,7 @@ public class WebSecurityConfig {
   @Bean
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173","http://localhost:5174"));
+    configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173","http://localhost:5174", "https://dawoodikram482.github.io/Code_Generation_Frontend"));
     configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
     configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
     configuration.setAllowedHeaders(List.of("*"));
